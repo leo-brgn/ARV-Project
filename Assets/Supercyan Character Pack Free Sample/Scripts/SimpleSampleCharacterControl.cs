4915615a659345dd0a101e2848d77e5152644313
leo <leo.brignone@gmail.com>
2024-02-07 19:42:32 +0100

    Adding cube to lvl 1

--- Assets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
+++ Assets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
@@ -12,10 +12,12 @@ public class SimpleSampleCharacterControl : MonoBehaviour
         /// <summary>
         /// Character freely moves in the chosen direction from the perspective of the camera
         /// </summary>
-        Direct
+        Direct,
+        Joystick
     }
 
     [SerializeField] private float m_moveSpeed = 2;
+    [SerializeField] private DynamicJoystick m_joystick;
     [SerializeField] private float m_turnSpeed = 200;
     [SerializeField] private float m_jumpForce = 4;
 
@@ -125,7 +127,9 @@ public class SimpleSampleCharacterControl : MonoBehaviour
             case ControlMode.Tank:
                 TankUpdate();
                 break;
-
+            case ControlMode.Joystick:
+                JoystickUpdate();
+                break;
             default:
                 Debug.LogError("Unsupported state");
                 break;
@@ -167,6 +171,41 @@ public class SimpleSampleCharacterControl : MonoBehaviour
     {
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
+
+        Transform camera = Camera.main.transform;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            v *= m_walkScale;
+            h *= m_walkScale;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        if (direction != Vector3.zero)
+        {
+            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+
+            transform.rotation = Quaternion.LookRotation(m_currentDirection);
+            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
+
+            m_animator.SetFloat("MoveSpeed", direction.magnitude);
+        }
+
+        JumpingAndLanding();
+    }
+
+    private void JoystickUpdate()
+    {
+        float v = m_joystick.Vertical;
+        float h = m_joystick.Horizontal;
 
         Transform camera = Camera.main.transform;
 
